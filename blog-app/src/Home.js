2d159a10f9d2154981a864b5c558b88f956e5424
Davit Danielyan <DavDan1>
2022-09-02 11:22:01 +0200

    Creates change button

--- blog-app/src/Home.js
+++ blog-app/src/Home.js
@@ -8,16 +8,24 @@ const Home = () => {
     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Jon', id: 3 }
   ])
 
+  const [name, setName] = useState('Jon');
+
   const handleDelete = (id) => {
     const newBlogs = blogs.filter(blog => blog.id !== id);
     setBlogs(newBlogs);
   }
 
+  useEffect(() => {
+    console.log('use effect ran');
+    console.log(blogs);
+  }, [name])
+
   return (
     <div className="home">
       <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+      <button onClick={() => setName('Doe')}>Change name</button>
     </div>
   );
 }
- 
+
 export default Home;
