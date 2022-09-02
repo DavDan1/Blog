cf8e1766282171fec0047e8a3f02b0278ff9a5e2
Davit Danielyan <DavDan1>
2022-09-02 11:26:50 +0200

    Create database json file

--- blog-app/src/Home.js
+++ blog-app/src/Home.js
@@ -1,29 +1,27 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'Dav', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'Nick', id: 2 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Jon', id: 3 }
-  ])
-
-  const [name, setName] = useState('Jon');
-
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter(blog => blog.id !== id);
-    setBlogs(newBlogs);
-  }
+  const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
 
   useEffect(() => {
-    console.log('use effect ran');
-    console.log(blogs);
-  }, [name])
+    setTimeout(() => {
+      fetch('http://localhost:8000/blogs')
+        .then(res => {
+          return res.json();
+        })
+        .then(data => {
+          setIsPending(false);
+          setBlogs(data);
+        })
+    }, 1000);
+  }, [])
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
-      <button onClick={() => setName('Doe')}>Change name</button>
+      {isPending && <div>Loading...</div>}
+      {blogs && <BlogList blogs={blogs} />}
     </div>
   );
 }
